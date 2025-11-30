b23b7baede0e13a9160fb8fb732433a40f44ed95
neerajcli <128034389+neerajcli@users.noreply.github.com>
2025-11-30 13:45:55 +0530

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -11,5 +11,6 @@ def load_and_preprocess_data():
     plt.show()
     X = df['post']
     y = df['label']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.14, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
+
