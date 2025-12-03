0fc3fcd3a667c4d523a5181f1bc539743776d9f0
neerajcli <128034389+neerajcli@users.noreply.github.com>
2025-12-03 14:47:06 +0530

    Update vectorize_data.py

--- vectorize_data.py
+++ vectorize_data.py
@@ -1,15 +1,23 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 import matplotlib.pyplot as plt
+from typing import Tuple
 
-def vectorize_data(X_train, X_test):
-    vectorizer = TfidfVectorizer(stop_words='english')
-    X_train_vect = vectorizer.fit_transform(X_train)
-    X_test_vect = vectorizer.transform(X_test)
+def vectorize_data(X_train, X_test) -> Tuple:
+    vectorizer = TfidfVectorizer(
+        ngram_range=(1, 2),
+        max_features=8000,
+        max_df=0.95,
+        min_df=2
+    )
+    X_train_vec = vectorizer.fit_transform(X_train)
+    X_test_vec = vectorizer.transform(X_test)
     feature_names = vectorizer.get_feature_names_out()
-    total_features = X_train_vect.sum(axis=0).A1
-    top_features = sorted(zip(total_features, feature_names), reverse=True)[:10]
-    plt.barh([x[1] for x in top_features], [x[0] for x in top_features], color='blue')
-    plt.xlabel('Frequency')
-    plt.title('Top 10 Features (TF-IDF)')
+    total_weights = X_train_vec.sum(axis=0).A1
+    top_indices = total_weights.argsort()[::-1][:10]
+    top_features = [feature_names[i] for i in top_indices]
+    top_values = [total_weights[i] for i in top_indices]
+    plt.barh(top_features[::-1], top_values[::-1])
+    plt.xlabel("Total TF-IDF Weight")
+    plt.title("Top 10 Features (TF-IDF)")
     plt.show()
-    return X_train_vect, X_test_vect, vectorizer
+    return X_train_vec, X_test_vec, vectorizer
